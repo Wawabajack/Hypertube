3e92822cc7e8a716a2c675378f8021b585af3266
BROGGI Thomas <tbroggi@student.42.fr>
2019-05-14 13:29:34 +0200

    Update google.js

--- server/routes/OmniAuth/google.js
+++ server/routes/OmniAuth/google.js
@@ -10,8 +10,8 @@ router.use(cors())
 
 const utils = require('../../models/utils')
 
-const GOOGLE_APP_ID = '63971582197-jljq44mimmtrpd6t6rat6gcbf6qe4qi8.apps.googleusercontent.com'
-const GOOGLE_APP_SECRET = 'gGE-ScIgjS9oVXMibldhsNnx'
+const GOOGLE_APP_ID = 'XXXXXX'
+const GOOGLE_APP_SECRET = 'XXXXXX'
 
 passport.serializeUser((user, done) => { done(null, user) })
 
@@ -20,7 +20,7 @@ passport.deserializeUser((id, done) => { User.findById(id).then((user) => { done
 passport.use(new GoogleStrategy({
         clientID: GOOGLE_APP_ID,
         clientSecret: GOOGLE_APP_SECRET,
-        callbackURL: "http://localhost:4000/auth/google/redirect",
+        callbackURL: "XXXXXX",
     }, (accessToken, refreshToken, profile, done) => {
         mongodb.collection('user').findOne({ $or: [{ login: profile._json.name }, { email: profile._json.email }, { authgoogleId: profile._json.sub }] })
             .then(user => {
@@ -39,7 +39,7 @@ passport.use(new GoogleStrategy({
 
 router.get('/', passport.authenticate('google', { scope: ['profile', 'email'] }))
 
-router.get('/redirect', passport.authenticate('google', { failureRedirect: 'http://localhost:8080/login?error=1' }), (req, res) => {
+router.get('/redirect', passport.authenticate('google', { failureRedirect: 'http://XXXXXX/login?error=1' }), (req, res) => {
     return new Promise((fullfil, reject) => {
         const data = {}
         data.params = req.user
@@ -47,7 +47,7 @@ router.get('/redirect', passport.authenticate('google', { failureRedirect: 'http
     })
     .then(utils.generateKey)
     .then(user.updateAuthenticatedToken)
-    .then(data => { res.redirect(`http://localhost:8080/login?loggin=${data.params.login}&key=${data.params.key}`) })
+    .then(data => { res.redirect(`http://XXXXXX/login?loggin=${data.params.login}&key=${data.params.key}`) })
 })
 
 module.exports = router

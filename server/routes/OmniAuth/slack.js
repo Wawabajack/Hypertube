b400240d8728a7a96e89a539db7b098e5a8fcbb9
BROGGI Thomas <tbroggi@student.42.fr>
2019-05-14 13:30:24 +0200

    Update slack.js

--- server/routes/OmniAuth/slack.js
+++ server/routes/OmniAuth/slack.js
@@ -9,8 +9,8 @@ router.use(cors())
 
 const utils = require('../../models/utils')
 
-const SLACK_APP_ID = '179072718960.603364952754'
-const SLACK_APP_SECRET = 'a8d8c3ca89608d149fa9751df80e01c7'
+const SLACK_APP_ID = 'XXXXXX'
+const SLACK_APP_SECRET = 'XXXXXX'
 
 passport.serializeUser((user, done) => { done(null, user) })
 
@@ -20,7 +20,7 @@ passport.use(new SlackStrategy({
         clientID: SLACK_APP_ID,
         clientSecret: SLACK_APP_SECRET,
         scope: 'identity.basic identity.email identity.avatar',
-        callbackURL: "http://localhost:4000/auth/slack/redirect"
+        callbackURL: "XXXXXX"
     }, (accessToken, refreshToken, profile, done) => {
         mongodb.collection('user').findOne({ $or: [{ login: profile.user.displayName }, { email: profile.user.email }, { authSlackId: profile.user.id }] })
             .then(user => {
@@ -39,7 +39,7 @@ passport.use(new SlackStrategy({
 
 router.get('/', passport.authenticate('slack'))
 
-router.get('/redirect', passport.authenticate('slack', { failureRedirect: 'http://localhost:8080/login?error=1' }), (req, res) => {
+router.get('/redirect', passport.authenticate('slack', { failureRedirect: 'http://XXXXXX/login?error=1' }), (req, res) => {
     return new Promise((fullfil, reject) => {
         const data = {}
         data.params = req.user
@@ -47,7 +47,7 @@ router.get('/redirect', passport.authenticate('slack', { failureRedirect: 'http:
     })
     .then(utils.generateKey)
     .then(user.updateAuthenticatedToken)
-    .then(data => { res.redirect(`http://localhost:8080/login?loggin=${data.params.login}&key=${data.params.key}`) })
+    .then(data => { res.redirect(`http://XXXXXX/login?loggin=${data.params.login}&key=${data.params.key}`) })
 })
 
 module.exports = router

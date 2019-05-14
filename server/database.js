9e2f987aa588a8e7e9cb94b13e76d74ab17b8860
BROGGI Thomas <tbroggi@student.42.fr>
2019-05-14 13:25:43 +0200

    Update database.js

--- server/database.js
+++ server/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/Hypertube', { useNewUrlParser: true })
-    .then(() => { console.info('Hypertube database connected on port: 27017')})
+mongoose.connect('mongodb://XXXXXX/Hypertube', { useNewUrlParser: true })
+    .then(() => { console.info('Hypertube database connected on port: XXXXXX')})
     .catch(err => { console.error(err) })
 
 global.mongodb = mongoose.connection
